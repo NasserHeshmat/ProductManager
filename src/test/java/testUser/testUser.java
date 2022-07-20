12a523e4d2411eb25f995a135ea9bfca6f24f721
MohamedM33 <mohamed.mohamed15@vodafone.com>
2022-07-20 15:32:24 +0200

    addding H2 database

---
+++ src/test/java/testUser/testUser.java
@@ -0,0 +1,14 @@
+package testUser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+public class testUser {
+
+	@Test
+	public void addUser() {
+		// TODO Auto-generated method stub
+
+	}
+}
